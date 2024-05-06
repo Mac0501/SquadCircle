3300b45ab42154b7ea2ac86f36cf79d8de67212d
Mac0501 <marc.schmengler@gmail.com>
2024-05-06 11:27:34 +0000

    fixed launch file for sanic

--- launcher.py
+++ launcher.py
@@ -2,11 +2,10 @@ from app.backend import app
 
 
 
-if __name__ == "__main__":
-    app.run(
-            debug=True,
-            noisy_exceptions=True,
-            access_log=True,
-            register_sys_signals=True,
-            single_process=True,
+app.run(
+        debug=True,
+        noisy_exceptions=True,
+        access_log=True,
+        register_sys_signals=True,
+        single_process=True,
         )
